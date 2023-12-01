e5ab6faebe64a7ab906ffe648e6dfd9930496109
Gustavs Fricis Jankavs <gfjankavs@tutamail.com>
2023-12-01 12:41:59 +0200

    Final adjustments

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,12 +2,17 @@ import Link from "next/link";
 
 const NotFound = () => {
     return (
-        <main className="max-w-screen-xl mx-auto md:pt-64 pt-48 p-4">
-            <h1 className="text-center md:text-9xl text-6xl font-bold">404</h1>
-            <h2 className="text-center md:text-4xl text-2xl font-semibold">
-                This page could not be found
-            </h2>
-            <Link href="/">Go Back To Home</Link>
+        <main className="flex flex-col justify-center gap-4 max-w-screen-xl mx-auto md:pt-64 pt-48 p-4">
+            <h1 className="text-center md:text-6xl text-3xl font-bold">404</h1>
+            <h3 className="text-center md:text-4xl text-2xl font-semibold">
+                This page does not exist
+            </h3>
+            <Link
+                href="/"
+                className="inline-block text-center md:text-4xl text-2xl"
+            >
+                Go Back To Home
+            </Link>
         </main>
     );
 };
